76abad22d07337476d78e8a80b3c72db0960e636
eclipse_user <eclipse_user@atbeijing.com>
2019-07-07 11:06:58 +0800

    six update by testgit

--- src/main/java/com/atbeijing/git/happy.java
+++ src/main/java/com/atbeijing/git/happy.java
@@ -2,28 +2,10 @@ package com.atbeijing.git;
 
 public class happy {
 	public static void main(String[] args) {
-		System.out.println("main...");
-		System.out.println("testgit1");
-		System.out.println("testgit2");
-		System.out.println("testgit222");
-		System.out.println("testgit222");
-		System.out.println("testgit222");
-		System.out.println("testgit222");
-		System.out.println("testgit222");
-		System.out.println("testgit222");
-	
-		System.out.println("aaaaaaaaaa");
-		System.out.println("aaaaaaaaaa");
-		System.out.println("aaaaaaaaaa");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
-		System.out.println("ccccccccccccc");
+		int i=0;
+		int j=i;
+		int k=j;
+		int l=k;
 
 	}
 }
